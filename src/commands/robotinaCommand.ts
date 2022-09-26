3140396defc4a6ac46f9b597b40bcb97d9d4ffbb
redotomi <tomy.redondo001@gmail.com>
2022-09-26 15:11:08 -0300

    Create Robotina

---
+++ src/commands/robotinaCommand.ts
@@ -0,0 +1,16 @@
+import { AckFn, App, RespondFn, SayFn, SlashCommand } from '@slack/bolt'
+import { StringIndexed } from '@slack/bolt/dist/types/helpers'
+
+interface Command {
+  command: SlashCommand
+  ack: AckFn<string>
+  say: SayFn
+  respond: RespondFn
+}
+
+export default async function robotinaCommand(
+  { command, ack, say, respond }: Command,
+  app: App<StringIndexed>
+) {
+  await ack()
+
