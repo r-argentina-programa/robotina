6188f555dfc62d98150257d71ecaa32839dee960
Roman <Roman.Mettler@gmail.com>
2022-09-26 11:19:41 -0300

    Add eslint and prettier as dev dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-import { App } from "@slack/bolt";
+import { App } from '@slack/bolt'
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.APP_TOKEN,
-});
+})
 
+;(async () => {
+  // Start your app
+  await app.start()
 
-(async () => {
-    // Start your app
-    await app.start();
-  
-    console.log("🤖 Robotina app is running!");
-  })();
+  console.log('🤖 Robotina app is running!')
+})()
