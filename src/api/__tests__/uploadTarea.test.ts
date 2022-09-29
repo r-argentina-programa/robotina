54f21af37024c61d1014fda7bc2fa1343b806ad9
redotomi <tomy.redondo001@gmail.com>
2022-09-29 15:42:22 -0300

    Implement the placeholder to the upload homework

---
+++ src/api/__tests__/uploadTarea.test.ts
@@ -0,0 +1,16 @@
+import { expect, jest, test } from '@jest/globals'
+import { uploadTarea } from '../uploadTarea'
+
+describe('uploadTarea test', () => {
+  it('should return the correct values', async () => {
+    const returnedValues = await uploadTarea('mockId', 'mockTarea', '12')
+    const expectedValues = {
+      tarea: 'mockTarea',
+      user_id: `oauth2|slack|${process.env.SLACK_TEAM_NAME}-mockId`,
+      slack_user_id: 'mockId',
+      class_number: '12',
+    }
+
+    expect(returnedValues).toEqual(expectedValues)
+  })
+})
