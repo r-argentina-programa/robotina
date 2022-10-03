d0dea85d0ac6935c2317650615250dba8ac39a5b
redotomi <tomy.redondo001@gmail.com>
2022-10-03 11:33:19 -0300

    Create a block for wrong format submissions

---
+++ src/blocks/wrongFormatBlock.ts
@@ -0,0 +1,19 @@
+export const wrongFormatBlock = [
+  {
+    type: 'header',
+    text: {
+      type: 'plain_text',
+      text: '❌ Formato inválido.',
+    },
+  },
+  {
+    type: 'divider',
+  },
+  {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: 'La tarea que enviaste está en un formato equivocado, si estás en la clase 4 o menos tenés que enviar la tarea como bloque de código y a partir de la clase 5 tenés que enviar el link de github.',
+    },
+  },
+]
