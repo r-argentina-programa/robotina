73558433022fbca902af963011244bc486f1fc92
redotomi <tomy.redondo001@gmail.com>
2022-10-06 11:38:06 -0300

    Update uploadTarea to check if the user exist before POST request

--- src/api/uploadTarea.ts
+++ src/api/uploadTarea.ts
@@ -9,18 +9,42 @@ export const uploadTarea = async (
   email: string | undefined
 ) => {
   const authOUserId = `oauth2|slack|${process.env.SLACK_TEAM_ID}-${userId}`
-  const { data } = await axios.post(
-    `${process.env.API_URL}/api/submission/bot`,
-    {
-      lessonId: classNumber,
-      userId: authOUserId,
-      firstName,
-      lastName,
-      email,
-      delivery,
-    }
-  )
 
-  console.log(data)
+  try {
+    if (await getExistentUser(authOUserId)) {
+      const { data } = await axios.post(
+        `${process.env.API_URL}/api/submission/bot`,
+        {
+          lessonId: classNumber,
+          userId: authOUserId,
+          delivery,
+        }
+      )
+
+      return data
+    } else {
+      const { data } = await axios.post(
+        `${process.env.API_URL}/api/submission/bot`,
+        {
+          lessonId: classNumber,
+          userId: authOUserId,
+          firstName,
+          lastName,
+          email,
+          delivery,
+        }
+      )
+
+      return data
+    }
+  } catch (error) {
+    throw new Error()
+  }
+}
+
+const getExistentUser = async (authUserId: string) => {
+  const { data } = await axios.get(
+    `${process.env.API_URL}/api/user/id/${authUserId}`
+  )
   return data
 }
