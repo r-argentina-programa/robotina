7964c2265ecb64f229068bacf8dadeb2b57d5b61
redotomi <tomy.redondo001@gmail.com>
2022-10-26 14:57:24 -0300

    Create message reaction event

---
+++ src/events/messageReaction.ts
@@ -0,0 +1,69 @@
+import { App, ReactionAddedEvent } from '@slack/bolt'
+import { WebClient } from '@slack/web-api/dist/WebClient'
+import { createThread, ICreateThread } from '../api/createThread'
+import { uploadTarea } from '../commands/tarea/uploadTarea'
+import { validateChannelName } from '../utils/validateChannelName'
+
+interface IReactionAddedEvent {
+  event: ReactionAddedEvent | any
+  client: WebClient
+}
+
+const submitWithMessageReactionFunction = async ({
+  client,
+  event,
+}: IReactionAddedEvent) => {
+  if (event.reaction === 'robot_face') {
+    const { user } = await client.users.info({
+      user: event.item_user,
+    })
+    if (!user) {
+      throw new Error('User not found')
+    }
+    const { channel } = await client.conversations.info({
+      channel: event.item.channel,
+    })
+    if (!channel) {
+      throw new Error('Channel not found')
+    }
+
+    const message = await client.conversations.history({
+      latest: event.item.ts,
+      channel: event.item.channel,
+      limit: 1,
+      inclusive: true,
+    })
+    if (!message) {
+      throw new Error('Message not found')
+    }
+
+    const classNumber = validateChannelName(channel?.name as string)
+    if (typeof classNumber !== 'string') {
+      throw new Error('Wrong channel name')
+    }
+    const messageText = message.messages![0]!.text as string
+
+    const tarea = await uploadTarea({
+      classNumber,
+      userId: user.id as string,
+      delivery: messageText,
+      firstName: user.profile!.first_name,
+      lastName: user.profile!.last_name,
+      email: user.profile!.email,
+    })
+
+    const thread: ICreateThread = {
+      authorId: <string>process.env.BOT_ID,
+      studentId: tarea.fkStudentId,
+      text: messageText,
+      timestamp: message.messages![0]!.ts as string,
+      taskId: tarea.fkTaskId,
+    }
+    await createThread(thread)
+  }
+}
+
+export const submitWithMessageReaction = (app: App) => {
+  const MESSAGE_REACTION_EVENT = 'reaction_added'
+  app.event(MESSAGE_REACTION_EVENT, submitWithMessageReactionFunction)
+}
