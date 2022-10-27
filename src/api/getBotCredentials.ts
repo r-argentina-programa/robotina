a8dafddaf661db0b5b4c87118c97b674953d6e00
Roman Mettler <59817562+Innolize@users.noreply.github.com>
2022-10-27 13:14:44 -0300

    Move getBotCredentials to its own file

---
+++ src/api/getBotCredentials.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from 'axios'
+
+interface IGetCredential {
+  access_token: string
+  expires_in: number
+  token_type: string
+}
+export const getBotCredentials = async () => {
+  console.log('Getting Auth0 credentials')
+  try {
+    const { data } = (await axios.post(
+      `${<string>process.env.AUTH0_DOMAIN}/oauth/token`,
+      {
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: process.env.AUTH0_AUDIENCE,
+        grant_type: process.env.GRANT_TYPE,
+        scope: process.env.AUTH0_SCOPE,
+        username: process.env.AUTH0_USERNAME,
+        password: process.env.AUTH0_PASSWORD,
+      }
+    )) as AxiosResponse<IGetCredential>
+    return data
+  } catch (err) {
+    throw new Error('Bot authentication failed')
+  }
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import axios, {
   AxiosInstance,
   AxiosRequestConfig,
 } from 'axios'
+import { getBotCredentials } from './getBotCredentials'
 
 type IAxiosError = AxiosError & {
   config: {
@@ -11,36 +12,9 @@ type IAxiosError = AxiosError & {
   }
 }
 
-interface IGetCredential {
-  access_token: string
-  expires_in: number
-  token_type: string
-}
-
 export const marketplaceApi = axios.create({
   baseURL: process.env.API_URL,
 })
-
-const getAuth0BotCredentials = async () => {
-  console.log('Getting Auth0 credentials')
-  try {
-    const { data } = (await axios.post(
-      `${<string>process.env.AUTH0_DOMAIN}/oauth/token`,
-      {
-        client_id: process.env.AUTH0_CLIENT_ID,
-        client_secret: process.env.AUTH0_CLIENT_SECRET,
-        audience: process.env.AUTH0_AUDIENCE,
-        grant_type: process.env.GRANT_TYPE,
-        scope: process.env.AUTH0_SCOPE,
-        username: process.env.AUTH0_USERNAME,
-        password: process.env.AUTH0_PASSWORD,
-      }
-    )) as AxiosResponse<IGetCredential>
-    return data
-  } catch (err) {
-    throw new Error('Bot authentication failed')
-  }
-}
 
 const setMarketplaceApiAuthorization = (
   api: AxiosInstance,
@@ -65,7 +39,7 @@ const interceptorRequestOnFail = async (error: IAxiosError) => {
   const originalRequest = error.config
   if (error.response?.status === 401 && !originalRequest.retry) {
     originalRequest.retry = true
-    const newToken = await getAuth0BotCredentials()
+    const newToken = await getBotCredentials()
     setMarketplaceApiAuthorization(marketplaceApi, newToken.access_token)
     setRequestConfigAuthorization(originalRequest, newToken.access_token)
     return axios(originalRequest)
