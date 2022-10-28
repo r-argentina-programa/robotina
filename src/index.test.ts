bf5ec436e5cbab1dfefd7af21c6f1992ea77d9b7
Roman Mettler <59817562+Innolize@users.noreply.github.com>
2022-10-28 12:05:01 -0300

    Remove file index.tests.ts

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Demo tests for github actions', () => {
-  it('1 + 2 should be equal to 3', () => {
-    const EXPECTED_RESPONSE = 3
-    const test = 1 + 2
-    expect(test).toEqual(EXPECTED_RESPONSE)
-  })
-})
