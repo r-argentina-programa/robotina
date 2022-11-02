ef51b0d868ddbd1f7018a38fe95cb7f9d51299b7
Roman Mettler <59817562+Innolize@users.noreply.github.com>
2022-11-02 11:28:46 -0300

    Create tests for new marketplace endpoints

--- src/api/getTasks.ts
+++ src/api/getTasks.ts
@@ -3,7 +3,7 @@ import { marketplaceApi } from '.'
 import { ITask } from '../interfaces/marketplaceApi/task'
 
 export const getTasks = async (classNumber: string) => {
-  const { data } = (await marketplaceApi(
+  const { data } = (await marketplaceApi.get(
     `/api/task?filter[lessonId]=${classNumber}`
   )) as AxiosResponse<ITask[]>
   return data
--- src/api/getUser.ts
+++ src/api/getUser.ts
@@ -1,9 +1,9 @@
-import { AxiosResponse} from 'axios'
+import { AxiosResponse } from 'axios'
 import { marketplaceApi } from '.'
 import { IUser } from '../interfaces/marketplaceApi/user'
 
 export const getUser = async (externalId: string) => {
-  const { data } = (await marketplaceApi(
+  const { data } = (await marketplaceApi.get(
     `/api/user?filter[externalId]=${externalId}&include[student]=true`
   )) as AxiosResponse<IUser[]>
   return data
