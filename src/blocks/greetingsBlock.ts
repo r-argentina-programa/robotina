0a3af83bc80ebdcfd6da5031d53bcfec7862ef14
Roman Mettler <59817562+Innolize@users.noreply.github.com>
2022-11-08 09:22:34 -0300

    Implement block builder in greetings block event

--- src/blocks/greetingsBlock.ts
+++ src/blocks/greetingsBlock.ts
@@ -1,42 +1,23 @@
-export const greetingsBlock = [
-  {
-    type: 'header',
-    text: {
-      type: 'plain_text',
-      text: 'Bienvenido a r-argentina-programa!',
-      emoji: true,
-    },
-  },
-  {
-    type: 'divider',
-  },
-  {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: 'Este es un curso para aprender JavaScript desde 0, gratis y en español.',
-    },
-  },
-  {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: 'Se recomienda hacer las tareas de cada clase y postearlas en su canal correspondiente, así alguno de los mentores o alumnos más avanzados pueden correjirlas y ayudarlos',
-    },
-  },
-  {
-    type: 'header',
-    text: {
-      type: 'plain_text',
-      text: 'Todavía tenés preguntas?',
-      emoji: true,
-    },
-  },
-  {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: 'Mirá las preguntas frecuentes <https://argentinaprograma.com/faq|*aca*> !',
-    },
-  },
-]
+import { Message, Blocks } from 'slack-block-builder'
+
+export const greetingsBlock = (channel: string) =>
+  Message({ channel, text: 'greeting-block-alter' })
+    .blocks(
+      Blocks.Header({
+        text: 'Bienvenido a r-argentina-programa!',
+      }),
+      Blocks.Divider(),
+      Blocks.Section({
+        text: 'Este es un curso para aprender JavaScript desde 0, gratis y en español.',
+      }),
+      Blocks.Section({
+        text: "Se recomienda hacer las tareas de cada clase y postearlas en su canal correspondiente, así alguno de los mentores o alumnos más avanzados pueden correjirlas y ayudarlos'",
+      }),
+      Blocks.Header({
+        text: 'Todavía tenés preguntas?',
+      }),
+      Blocks.Section({
+        text: 'Mirá las preguntas frecuentes <https://argentinaprograma.com/faq|*aca*> !',
+      })
+    )
+    .buildToObject()
