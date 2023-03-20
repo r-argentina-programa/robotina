f3e2f31f3216e07dcec1a6ce8054201b874222fd
Martin Novoa <martinnovoar2@gmail.com>
2023-03-20 10:53:06 -0300

    Change validation for threads to use new message

--- src/utils/validateTaskSubmission.ts
+++ src/utils/validateTaskSubmission.ts
@@ -1,8 +1,5 @@
 export const isTaskSubmission = (text: string) => {
   const SUBMISSION_NAME = 'Tarea'
   const submission = text.slice(0, 40).split(' ')
-  if (submission[1] === SUBMISSION_NAME) {
-    return true
-  }
-  return false
+  return submission[0] === SUBMISSION_NAME;
 }
