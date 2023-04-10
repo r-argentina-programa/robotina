512e8076a31e34532ff34c7c6b7a2da881a55274
Miguel Hernandez <miguelherzdev@gmail.com>
2023-04-10 09:28:28 -0400

    fix auth0-id in createAuth0Id

--- src/utils/createAuth0Id.ts
+++ src/utils/createAuth0Id.ts
@@ -1,5 +1,5 @@
 export const createAuth0Id = (id: string) => {
-  const BASE_AUTH0_ID = `oauth2|slack|`
+  const BASE_AUTH0_ID = `oauth2|sign-in-with-slack`
   const TEAM_ID = process.env.SLACK_TEAM_ID
   return `${BASE_AUTH0_ID}${TEAM_ID}-${id}`
 }
