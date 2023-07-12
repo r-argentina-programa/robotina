3365ede0191ab4f04d1a611921c2abde0939b1a1
Alan <tiltedveggie@outlook.kr>
2023-07-12 16:23:45 -0300

    Create a link extractor for submissions

--- src/utils/extractOnlySubmission.ts
+++ src/utils/extractOnlySubmission.ts
@@ -1,10 +1,17 @@
 import { extractCodeBlockFromSubmission } from './extractCodeBlockFromSubmission';
+import { extractLinkFromSubmission } from './extractLinkFromSubmission';
 
 const isBlockOfCode = (text: string): boolean => text.includes('```');
+
+const isLink = (text: string): boolean => text.includes('github.com');
 
 export const extractOnlySubmission = (message: string): string => {
   if (isBlockOfCode(message)) {
     return extractCodeBlockFromSubmission(message);
+  }
+
+  if (isLink(message)) {
+    return extractLinkFromSubmission(message);
   }
 
   return '';
